adfe508fe9f1e98103d300865f1c69e0fa516d9f
Zeqli <l.caecar@gmail.com>
2018-02-08 23:14:07 -0600

    Added node sample project - Module Basic

---
+++ n-3-2-module-basics/app.js
@@ -0,0 +1,10 @@
+console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+console.log(user);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`);
